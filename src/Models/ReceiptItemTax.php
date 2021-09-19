e020d9403c93366a54c32a60cf6244df18804b9e
Gilbert Rutatiina <rutatiina@ruginem.org>
2021-09-19 18:44:25 +0300

    Update - tenant scoping - update globals

--- src/Models/ReceiptItemTax.php
+++ src/Models/ReceiptItemTax.php
@@ -4,7 +4,7 @@ namespace Rutatiina\Receipt\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Activitylog\Traits\LogsActivity;
-use App\Scopes\TenantIdScope;
+use Rutatiina\Tenant\Scopes\TenantIdScope;
 
 class ReceiptItemTax extends Model
 {
